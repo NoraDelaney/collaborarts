ca68c912de80faa42a1b36f4db2c38fe73e2135a
Nora Delaney <nora.delaney@gmail.com>
2015-06-15 22:50:34 -0400

    Added flash messages and rescue PG::Error

--- schema.sql
+++ schema.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   full_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  location VARCHAR(255),
-  artist_type VARCHAR(255),
-  looking_for VARCHAR(255),
-  description VARCHAR(2048)
+  email VARCHAR(255) UNIQUE NOT NULL,
+  location VARCHAR(255) NOT NULL,
+  artist_type VARCHAR(255) NOT NULL,
+  looking_for VARCHAR(255) NOT NULL,
+  description VARCHAR(2048) NOT NULL,
 );
--- server.rb
+++ server.rb
@@ -1,13 +1,19 @@
 require 'sinatra'
 require 'pg'
 require 'pry'
+require 'sinatra/flash'
+
+enable :sessions
 
 
 def db_connection
   begin
     connection = PG.connect(dbname: "collaborarts")
-#  rescue PG::UniqueViolation figure this out
     yield(connection)
+  rescue PG::Error
+    flash[:warning] = "Your email has already been used."
+    redirect '/collaborarts/form'
+
   ensure
     connection.close
   end
@@ -27,14 +33,16 @@ end
 
 post '/collaborarts/form' do
 $looking_for = ""
+$my_email = ""
   db_connection do |conn|
     conn.exec_params("INSERT INTO users (full_name, email, location, artist_type, looking_for, description) VALUES ($1, $2, $3, $4, $5, $6)", submission)
-  end
+end
 
  $looking_for = submission[4]
+ $my_email = submission[1]
+
   redirect '/collaborarts/results'
 
-  binding.pry
 end
 
 
@@ -43,8 +51,21 @@ get '/collaborarts/results' do
 db_connection do |conn|
   results = conn.exec("SELECT full_name, email, location FROM users WHERE artist_type = $1",  [$looking_for])
   end
-results = results.to_a
-erb :results, locals: {results: results}
+
+  results = results.to_a
+
+  if results.empty?
+    flash[:alert] = "Sorry, there are no matches for that category."
+    redirect '/collaborarts/form'
+  end
+
+  if results[-1]["email"] == $my_email
+    results.slice!(-1)
+  end
+
+
+
+  erb :results, locals: {results: results}
 end
 
 get '/collaborarts/:full_name' do
@@ -57,15 +78,4 @@ get '/collaborarts/:full_name' do
     info = info.to_a
 
   erb :show, locals: {name: params[:full_name], info: info}
-  end
-
-  # get '/movies/:id' do
-  # movie_info = db_connection { |conn| conn.exec("SELECT movies.title AS movie, movies.year AS year, movies.rating AS rating, genres.name AS genre, studios.name AS studio, actors.name AS actor, cast_members.character AS role, movies.synopsis AS synopsis
-  #   FROM movies
-  #   LEFT JOIN cast_members ON movies.id = cast_members.movie_id
-  #   LEFT JOIN genres ON movies.genre_id = genres.id
-  #   LEFT JOIN studios ON movies.studio_id = studios.id
-  #   LEFT JOIN actors ON cast_members.actor_id = actors.id
-  #   WHERE movies.title = $1", [params[:id]] )}
-  # erb :'movies/show', locals: { movie: params[:id], movie_info: movie_info }
-# end
+end
